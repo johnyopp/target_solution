8d10a6f58a113f0175ece2b2f90c34467ebab12f
John Yopp <jyopp@thoughtworks.com>
2017-04-11 20:37:24 -0700

    first call

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var pg = require('pg');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -17,4 +19,18 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+app.get('/products/:productId', function (request, response) {
+  var product = request.params.productId;
+  
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query("SELECT attributes->'currency' as currency, attributes->'price' as price FROM products WHERE product_id='" + product + "'", function(err, result) {
+      done();
 
+      // Handle any errors.
+      if (err) return response.json(err);
+
+      // Return result
+      return response.json(result.rows);
+    });
+  });
+});
