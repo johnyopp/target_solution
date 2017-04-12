1754a3e205d838ebbf125602e60d9ec0a3d03438
John Yopp <jyopp@thoughtworks.com>
2017-04-12 07:10:56 -0700

    test header change

--- index.js
+++ index.js
@@ -71,13 +71,10 @@ console.log('HERE 3');
 });
 
 function getProductName(product, cb) {
-	var  call_headers = {
-      'Content-Type': 'application/json',
-    };
     https.get({
         host: 'redsky.target.com',
         path: '/v1/pdp/tcin/' + product + '?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics',
-        headers: call_headers
+        headers: 'Content-Type': 'application/json'
     };
 
     }, function(res) {
