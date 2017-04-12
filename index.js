1bf8814e1ba5054b26499724b8b897d36189fc60
John Yopp <jyopp@thoughtworks.com>
2017-04-11 21:04:49 -0700

    blended call, first attempt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var pg = require('pg');
 var bodyParser = require('body-parser');
 var app = express();
+var http = require('http');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -22,15 +23,59 @@ app.listen(app.get('port'), function() {
 app.get('/products/:productId', function (request, response) {
   var product = request.params.productId;
   
+  getProductName(product, function(err, name){
+
+
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query("SELECT attributes->'currency' as currency, attributes->'price' as price FROM products WHERE product_id='" + product + "'", function(err, result) {
+    client.query("SELECT attributes->'price' as value, attributes->'currency' as currency_code FROM products WHERE product_id='" + product + "'", function(err, result) {
       done();
 
       // Handle any errors.
       if (err) return response.json(err);
 
       // Return result
-      return response.json(result.rows);
+      return response.json({"id" : product,"name": name,"current_price": result.rows});
+
     });
   });
+
+
+
+  	});
+
+
 });
+
+function getProductName(product, cb) {
+    http.get({
+        host: 'redsky.target.com',
+        path: '/v1/pdp/tcin/' + product + '?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics'
+    }, function(res) {
+        // explicitly treat incoming data as utf8 (avoids issues with multi-byte chars)
+        res.setEncoding('utf8');
+
+        // incrementally capture the incoming response body
+        var body = '';
+        res.on('data', function(d) {
+            body += d;
+        });
+
+        // do whatever we want with the response once it's done
+        res.on('end', function() {
+            try {
+                var parsed = JSON.parse(body);
+            } catch (err) {
+                console.error('Unable to parse response as JSON', err);
+                return cb(err);
+            }
+
+            // pass the relevant data back to the callback
+            cb(null, parsed.product.item.product_description.title);
+        });
+    }).on('error', function(err) {
+        // handle errors with the request itself
+        console.error('Error with the request:', err.message);
+        cb(err);
+    });
+
+}
