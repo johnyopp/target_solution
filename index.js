9c6f60c97ee20e4cd3e40b8a2856fb0d75f78fc6
John Yopp <jyopp@thoughtworks.com>
2017-04-12 07:57:43 -0700

    test header change

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function getProductName(product, cb) {
     https.get({
 //        host: 'redsky.target.com',
 //        path: '/v1/pdp/tcin/' + product
-          host: 'maps.googleapis.com'
+          host: 'maps.googleapis.com',
           path: '/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key=AIzaSyBliTe19QOrCj12Lt1YbPDzi4I7MZJHqkk'
         }, function(res) {
         // explicitly treat incoming data as utf8 (avoids issues with multi-byte chars)
